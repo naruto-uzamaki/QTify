da62d2b6045e7b320b4b07ddd2af85b48cca6f14
naruto-uzamaki <kalavalarevanth@gmail.com>
2025-03-22 13:09:16 +0530

    Impl of Songs Section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -7,6 +7,8 @@ import { useEffect, useState } from 'react';
 import styles from './App.css';
 import { fetchGenreList, fetchNewAlbums, fetchSongs, fetchTopAlbums } from './api/api';
 import Section from './components/Section/Section';
+import FAQAccordion from './components/FAQ/FAQAccordion';
+import GenreSection from './components/GenreSection/GenreSection';
 
 
 function App() {
@@ -37,6 +39,12 @@ function App() {
       <div style={{marginBottom: '30px'}}>
         <Section data={newAlbums} title="New Albums" />
       </div>
+      <hr className={styles.divider} />
+      <div>
+        <GenreSection data={songs} title="Songs" genreList={genreList} />
+      </div>
+      <hr className={styles.divider} />
+      <FAQAccordion />
     </div>
   );
 }
--- qtify/src/helpers/helpers.js
+++ qtify/src/helpers/helpers.js
@@ -3,4 +3,13 @@ export const truncate = (string, length) => {
       return string.slice(0, length) + '...';
     }
     return string;
-  };
+};
+  
+export function sortByGenre(data, genreType){
+    if(genreType === 'all'){
+        return data;
+    }
+    return data.filter((item)=>{
+        return item.genre.key === genreType;
+    })
+};
