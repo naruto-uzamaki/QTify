5f25dee34914329e8d732f08f50874afa942c5e8
naruto-uzamaki <kalavalarevanth@gmail.com>
2025-03-22 12:09:03 +0530

    Impl of Top albums and new albums

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -3,12 +3,40 @@ import NavBar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 
 
+import { useEffect, useState } from 'react';
+import styles from './App.css';
+import { fetchGenreList, fetchNewAlbums, fetchSongs, fetchTopAlbums } from './api/api';
+import Section from './components/Section/Section';
+
+
 function App() {
+  const [topAlbums, setTopAlbums] = useState([]);
+  const [newAlbums, setNewAlbums] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const [genreList, setGenreList] = useState([]);
+  
+  const generatedata = async () => {
+    setTopAlbums(await fetchTopAlbums());
+    setNewAlbums(await fetchNewAlbums());
+    setSongs(await fetchSongs());
+    setGenreList(await fetchGenreList());
+  }
+
+
+  useEffect(() => {
+    generatedata();
+  }, [])
+
   return (
-    <div className="App">
-      
-      <NavBar />
+    <div className={styles.app}>
+      <NavBar searchData={songs}/>
       <Hero />
+      <div style={{marginBottom: '30px'}}>
+        <Section data={topAlbums} title="Top Albums" />
+      </div>
+      <div style={{marginBottom: '30px'}}>
+        <Section data={newAlbums} title="New Albums" />
+      </div>
     </div>
   );
 }
